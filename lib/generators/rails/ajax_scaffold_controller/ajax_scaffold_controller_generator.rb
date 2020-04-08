53b7e081231f8db5c89a6854c00178b852f10f65
rosew <rose@parksidesoftware.com>
2020-04-07 22:16:47 -0600

    init app

---
+++ lib/generators/rails/ajax_scaffold_controller/ajax_scaffold_controller_generator.rb
@@ -0,0 +1,13 @@
+# A modified version of rails scaffolding controller that uses modals for new/edit and ajax for create/update/delete
+
+require "rails/generators/rails/scaffold_controller/scaffold_controller_generator"
+
+class Rails::AjaxScaffoldControllerGenerator < Rails::Generators::ScaffoldControllerGenerator
+
+  hook_for :template_engine, as: :ajax_scaffold do |template_engine|
+    invoke template_engine unless options.api?
+  end
+
+  hook_for :test_framework, as: :ajax_scaffold
+
+end
